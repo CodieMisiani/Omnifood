911c28bd43bd7a2014a1e79193f728b3dd5fd7bb
CodieMisiani <nimrodmisiani1@gmail.com>
2025-10-31 02:04:16 +0300

    feat: implement Navigation component with routing links

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <>
+      <nav>
+        <NavLink to="/" activeClassName="active">
+          Home
+        </NavLink>
+        <NavLink to="/HowItWorks" activeClassName="active">
+          HowItWorks
+        </NavLink>
+        <NavLink to="/Meals" activeClassName="active">
+          Meals
+        </NavLink>
+        <NavLink to="/Testimonials" activeClassName="active">
+          Testimonials
+        </NavLink>
+        <NavLink to="/Pricing" activeClassName="active">
+          Pricing
+        </NavLink>
+      </nav>
+    </>
+  );
 };
 
 export default Navigation;

8a98666bee563719868d051435838cb70d576181
CodieMisiani <nimrodmisiani1@gmail.com>
2025-11-02 21:22:07 +0300

    style: implement Hero component with structured layout and content

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import Heroes from "../assets/hero.png"; // adjust path if different
+import { Link, NavLink } from "react-router-dom";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <>
+      <section class="section-hero">
+        <div class="hero">
+          <div class="hero-text-box">
+            <h1 class="heading-primary">
+              A healthy meal delivered to your door, every single day
+            </h1>
+            <p class="hero-description">
+              The smart 365-days-per-year food subscription that will make you
+              eat healthy again. Tailored to your personal tastes and
+              nutritional needs. We have delivered 250,000+ meals last year!
+            </p>
+            <Link to="#" class="btn">
+              Start Eating Well
+            </Link>
+            <Link to="#" class="btn">
+              Learn More &darr;
+            </Link>
+          </div>
+          <div class="hero-img-box">
+            <img
+              src={Heroes}
+              alt="Hero Picture"
+              className="object-contain w-[100%]"
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Hero;

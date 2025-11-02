4bbcc15cdcb7f00451de1ca88f236d31b1b9c57e
CodieMisiani <nimrodmisiani1@gmail.com>
2025-11-02 16:18:48 +0300

    feat: update Logo component to display Omnifood logo image

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import logo from "../assets/omnifood-logo.png"; // adjust path if different
 const Logo = () => {
-  return <div>Logo</div>;
+  return (
+    <div>
+      <img src={logo} alt="Omnifood logo" className="object-contain w-[20%]" />
+    </div>
+  );
 };
 
 export default Logo;

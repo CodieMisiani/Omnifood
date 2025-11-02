55106ae74789f5fc86903c31d7f210617d33ddbf
CodieMisiani <nimrodmisiani1@gmail.com>
2025-11-02 15:55:09 +0300

    refactor: restructure App component layout for better readability

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Header from "./Components/Header";
 const App = () => {
   return (
     <>
-      <header>
-        <Header />
-      </header>
-      <main className="min-h-screen flex items-center justify-center">
-        <h1 className="text-3xl font-bold">Omnifood — React + Tailwind</h1>
-      </main>
+      <div className="w-[43.75%] mx-auto">
+        <header>
+          <Header />
+        </header>
+        <main className="min-h-screen flex items-center justify-center">
+          <h1 className="text-3xl font-bold">Omnifood — React + Tailwind</h1>
+        </main>
+      </div>
     </>
   );
 };

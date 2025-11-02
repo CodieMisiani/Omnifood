edd672e3d4a0fdffa1eee851657c5e0b6408a39e
CodieMisiani <nimrodmisiani1@gmail.com>
2025-11-02 20:08:26 +0300

    style: update Navigation component structure and styling for consistency

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -4,18 +4,27 @@ import { Link, NavLink } from "react-router-dom";
 const Navigation = () => {
   return (
     <>
-      <nav className="flex items-center justify-between">
-        <div className="flex space-x-6">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/HowItWorks">HowItWorks</NavLink>
-          <NavLink to="/Meals">Meals</NavLink>
-          <NavLink to="/Testimonials">Testimonials</NavLink>
-          <NavLink to="/Pricing">Pricing</NavLink>
-        </div>
-        <div>
-          <button type="button" className="pl-5 pr-5">
-            Click Me
-          </button>
+      <nav class="nav-bar" className="flex items-center justify-between">
+        <div class="nav-container">
+          <div class="nav-containerlink">
+            <NavLink to="/HowItWorks" class="navlinks">
+              HowItWorks
+            </NavLink>
+            <NavLink to="/Meals" class="navlinks">
+              Meals
+            </NavLink>
+            <NavLink to="/Testimonials" class="navlinks">
+              Testimonials
+            </NavLink>
+            <NavLink to="/Pricing" class="navlinks">
+              Pricing
+            </NavLink>
+          </div>
+          <div>
+            <button type="button" class="btn">
+              TryForFree
+            </button>
+          </div>
         </div>
       </nav>
     </>

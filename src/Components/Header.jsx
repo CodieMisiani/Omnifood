6d63e8f248cc94e968192276fe6b515965865e2a
CodieMisiani <nimrodmisiani1@gmail.com>
2025-11-02 20:07:54 +0300

    style: fix whitespace in Header component for consistency

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ import Logo from "./Logo";
 const Header = () => {
   return (
     <>
-      <div className="w-full max-w-6xl mx-auto px-4 py-3 flex items-center gap-6">
+      <div className=" w-full max-w-6xl mx-auto px-4 py-3 flex items-center gap-6">
         <Logo />
         <Navigation />
       </div>

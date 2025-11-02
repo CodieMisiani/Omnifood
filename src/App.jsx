f972d777006ac15d7fb2e4860ac39dbda9be5974
CodieMisiani <nimrodmisiani1@gmail.com>
2025-11-02 21:23:14 +0300

    style: update App component layout and integrate Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-
 import Header from "./Components/Header";
+import Hero from "./Components/Hero";
 
 const App = () => {
   return (
     <>
-      <div className="w-[43.75%] mx-auto">
+      <div className="w-[70rem] mx-auto">
         <header>
           <Header />
         </header>
         <main className="min-h-screen flex items-center justify-center">
-          <h1 className="text-3xl font-bold">Omnifood — React + Tailwind</h1>
+          <Hero />
+          {/* <h1 className="text-3xl font-bold">Omnifood — React + Tailwind</h1> */}
         </main>
       </div>
     </>

52a1dde7123aee4869e7a220464b19a3bc2b5c58
CodieMisiani <nimrodmisiani1@gmail.com>
2025-11-02 20:08:11 +0300

    style: update logo image width for better visibility

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -3,7 +3,7 @@ import logo from "../assets/omnifood-logo.png"; // adjust path if different
 const Logo = () => {
   return (
     <div>
-      <img src={logo} alt="Omnifood logo" className="object-contain w-[20%]" />
+      <img src={logo} alt="Omnifood logo" className="object-contain w-[50%]" />
     </div>
   );
 };

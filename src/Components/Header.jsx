3bc74814da221077ce9686a4778d83ab21a37823
CodieMisiani <nimrodmisiani1@gmail.com>
2025-11-03 03:07:39 +0300

    style: enhance header layout by adding justify-between for improved alignment

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ import Logo from "./Logo";
 const Header = () => {
   return (
     <>
-      <div className=" w-full max-w-6xl mx-auto px-4 py-3 flex items-center gap-6">
+      <div className=" w-full max-w-6xl mx-auto px-4 py-3 flex items-center gap-6 flex items-center justify-between">
         <Logo />
         <Navigation />
       </div>

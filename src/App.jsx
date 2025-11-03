d101b7b7257ea1d155dc051184e370dc96d39996
CodieMisiani <nimrodmisiani1@gmail.com>
2025-11-03 03:07:06 +0300

    style: add space after section class for improved formatting consistency

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Hero from "./Components/Hero";
 const App = () => {
   return (
     <>
-      <section className="bg-[#fae5d3]">
+      <section className="bg-[#fae5d3] ">
         <header>
           <Header />
         </header>
